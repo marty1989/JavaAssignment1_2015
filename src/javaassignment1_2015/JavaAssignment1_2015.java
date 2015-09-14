1a96c0cff1a7ff63e784e988b2c38f792eef4540
Bryan <b.martin.89@hotmail.com>
2015-09-14 16:00:27 -0400

    Initial Commit

---
+++ src/javaassignment1_2015/JavaAssignment1_2015.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaassignment1_2015;
+
+/**
+ *
+ * @author c0564747
+ */
+public class JavaAssignment1_2015 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+/**************
+* Assignment # 1
+* Job Posting: FIFA - Java Engineer
+* https://career4.successfactors.com/career?career_ns=job_listing&company=EA&navBarLevel=JOB_SEARCH&rcm_site_locale=en_US&career_job_req_id=69842&jobPipeline=Indeed
+* JUnit - https://en.wikipedia.org/wiki/JUnit
+* IntelliJ IDEA - https://en.wikipedia.org/wiki/IntelliJ_IDEA
+* Eclipse - https://en.wikipedia.org/wiki/Eclipse_(software)
+* 
+* Vancouver - C$81,206
+* Toronto - C$76,768
+* Waterloo - C$78,765
+*/
