0b6bdce19a1f0df775036797cea51211b4ddbda7
Bryan <b.martin.89@hotmail.com>
2015-09-16 18:22:59 -0400

    Added new definitions for Java frameworks.

--- src/javaassignment1_2015/JavaAssignment1_2015.java
+++ src/javaassignment1_2015/JavaAssignment1_2015.java
@@ -22,10 +22,11 @@ public class JavaAssignment1_2015 {
 /**************
 * Assignment # 1
 * Job Posting: FIFA - Java Engineer
+* Career opportunity using Java with SQL, includes modular testing of code
 * https://career4.successfactors.com/career?career_ns=job_listing&company=EA&navBarLevel=JOB_SEARCH&rcm_site_locale=en_US&career_job_req_id=69842&jobPipeline=Indeed
-* JUnit - https://en.wikipedia.org/wiki/JUnit
-* IntelliJ IDEA - https://en.wikipedia.org/wiki/IntelliJ_IDEA
-* Eclipse - https://en.wikipedia.org/wiki/Eclipse_(software)
+* JUnit -  JUnit is a commonly used Java framework designed to write repeatable tests. JUnit is an instance of the xUnit family of testing frameworks.  https://en.wikipedia.org/wiki/JUnit
+* IntelliJ IDEA - IntelliJ IDEA is an Integrated Development Environment used for the Java, Scala, and Groovy languages. https://en.wikipedia.org/wiki/IntelliJ_IDEA
+* Eclipse - Eclipse is an open-source Integrated Development Environment mainly used for developing Java applications but can also used for other programming languages through use of plug-ins. https://en.wikipedia.org/wiki/Eclipse_(software)
 * 
 * Vancouver - C$81,206
 * Toronto - C$76,768
